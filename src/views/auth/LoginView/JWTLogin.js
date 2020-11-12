5b35c089db6513186d996c860a9413060411261d
José L. Gutiérrez <gutiecuador@gmail.com>
2020-11-12 18:05:38 -0500

    Initial commit

---
+++ src/views/auth/LoginView/JWTLogin.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import clsx from 'clsx';
+import * as Yup from 'yup';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import {
+  Box,
+  Button,
+  FormHelperText,
+  TextField,
+  makeStyles
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
+import useAuth from 'src/hooks/useAuth';
+import useIsMountedRef from 'src/hooks/useIsMountedRef';
+
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
+
+const JWTLogin = ({ className, ...rest }) => {
+  const classes = useStyles();
+  const { login } = useAuth();
+  const isMountedRef = useIsMountedRef();
+
+  return (
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+        submit: null
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string().email('Debe ser un email valido').max(255).required('El correo es requerido'),
+        password: Yup.string().max(255).required('La contraseña es requerida')
+      })}
+      onSubmit={async (values, {
+        setErrors,
+        setStatus,
+        setSubmitting
+      }) => {
+        try {
+          await login(values.email, values.password);
+
+          if (isMountedRef.current) {
+            setStatus({ success: true });
+            setSubmitting(false);
+          }
+        } catch (err) {
+          console.error(err);
+          if (isMountedRef.current) {
+            setStatus({ success: false });
+            setErrors({ submit: err.message });
+            setSubmitting(false);
+          }
+        }
+      }}
+    >
+      {({
+        errors,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+        touched,
+        values
+      }) => (
+        <form
+          noValidate
+          onSubmit={handleSubmit}
+          className={clsx(classes.root, className)}
+          {...rest}
+        >
+          <TextField
+            error={Boolean(touched.email && errors.email)}
+            fullWidth
+            autoFocus
+            helperText={touched.email && errors.email}
+            label="Email"
+            margin="normal"
+            name="email"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="email"
+            value={values.email}
+            variant="outlined"
+          />
+          <TextField
+            error={Boolean(touched.password && errors.password)}
+            fullWidth
+            helperText={touched.password && errors.password}
+            label="Contraseña"
+            margin="normal"
+            name="password"
+            onBlur={handleBlur}
+            onChange={handleChange}
+            type="password"
+            value={values.password}
+            variant="outlined"
+          />
+          {errors.submit && (
+            <Box mt={3}>
+              <FormHelperText error>
+                {errors.submit}
+              </FormHelperText>
+            </Box>
+          )}
+          <Box mt={2}>
+            <Button
+              color="secondary"
+              disabled={isSubmitting}
+              fullWidth
+              size="large"
+              type="submit"
+              variant="contained"
+            >
+              Ingresar
+            </Button>
+          </Box>
+        </form>
+      )}
+    </Formik>
+  );
+};
+
+JWTLogin.propTypes = {
+  className: PropTypes.string,
+};
+
+export default JWTLogin;
---
+++ src/views/errors/NotFoundView.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  makeStyles
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(3),
+    paddingTop: 80,
+    paddingBottom: 80
+  },
+  image: {
+    maxWidth: '100%',
+    width: 560,
+    maxHeight: 300,
+    height: 'auto'
+  }
+}));
+
+const NotFoundView = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const mobileDevice = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <Page
+      className={classes.root}
+      title="404: Not found"
+    >
+      <Container maxWidth="lg">
+        <Typography
+          align="center"
+          variant={mobileDevice ? 'h4' : 'h1'}
+          color="textPrimary"
+        >
+          404: La página que estas buscando no se encuentra en esta dirección.
+        </Typography>
+        <Typography
+          align="center"
+          variant="subtitle2"
+          color="textSecondary"
+        >
+          Puede que hayas llegado aqui por error, intente usar la navegación.
+        </Typography>
+        <Box
+          mt={6}
+          display="flex"
+          justifyContent="center"
+        >
+          <img
+            alt="Under development"
+            className={classes.image}
+            src="/static/images/undraw_page_not_found_su7k.svg"
+          />
+        </Box>
+        <Box
+          mt={6}
+          display="flex"
+          justifyContent="center"
+        >
+          <Button
+            color="secondary"
+            component={RouterLink}
+            to="/"
+            variant="outlined"
+          >
+            Regresar a el inicio
+          </Button>
+        </Box>
+      </Container>
+    </Page>
+  );
+};
+
+export default NotFoundView;
