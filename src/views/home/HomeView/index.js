8196a4035ea31efc9a2f0028138f27e1f493aa55
José L. Gutiérrez <gutiecuador@gmail.com>
2021-04-27 23:36:42 -0500

    add FAB button whatsapp

--- src/views/home/HomeView/index.js
+++ src/views/home/HomeView/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
 import Hero from './Hero';
+import Fab from '@material-ui/core/Fab';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 
-const useStyles = makeStyles(() => ({
-  root: {}
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(3),
+    background: 'green'
+  }
 }));
 
 const HomeView = () => {
@@ -16,6 +24,14 @@ const HomeView = () => {
       title="Bienvenido"
     >
       <Hero />
+    <Fab 
+      className={classes.fab} 
+      size="large"
+      color="primary" 
+      href="https://api.whatsapp.com/send?phone=+573186819767&text=Hola%21%20necesito%20m%C3%A1s%20informaci%C3%B3n%20."  target="_blank"
+      aria-label="Centro de Información">
+      <WhatsAppIcon />
+    </Fab>
     </Page>
   );
 };
